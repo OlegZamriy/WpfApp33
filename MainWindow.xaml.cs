7cc941ef7d81b05fae03e2cfea5f02f167944868
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 20:27:34 +0300

    1`

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,51 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp33
+namespace ArrayToFile
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SaveToFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            string arrayInput = ArrayInputTextBox.Text;
+            string[] elements = arrayInput.Split(',');
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter("array.txt"))
+                {
+                    foreach (string element in elements)
+                    {
+                        writer.WriteLine(element);
+                    }
+                }
+                StatusMessageLabel.Content = "Масив був успішно збережений у файл.";
+            }
+            catch (Exception ex)
+            {
+                StatusMessageLabel.Content = $"Помилка збереження масиву у файл: {ex.Message}";
+            }
+        }
+
+        private void LoadFromFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string[] elements = File.ReadAllLines("array.txt");
+                string arrayInput = string.Join(",", elements);
+                ArrayInputTextBox.Text = arrayInput;
+                StatusMessageLabel.Content = "Масив був успішно завантажений з файлу.";
+            }
+            catch (Exception ex)
+            {
+                StatusMessageLabel.Content = $"Помилка завантаження масиву з файлу: {ex.Message}";
+            }
         }
     }
 }
