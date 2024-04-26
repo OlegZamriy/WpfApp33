28b09db9f71408868d2354289a0886e1b097fdee
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 20:28:15 +0300

    1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,42 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp33
+namespace RandomNumbers
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GenerateNumbers_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            int[] numbers = new int[10000];
+            random.NextBytes(new byte[numbers.Length * sizeof(int)]);
+
+            int[] evenNumbers = numbers.Where(n => n % 2 == 0).ToArray();
+            int[] oddNumbers = numbers.Where(n => n % 2 != 0).ToArray();
+
+            WriteToFile("evenNumbers.txt", evenNumbers);
+            WriteToFile("oddNumbers.txt", oddNumbers);
+
+            DisplayStatistics(evenNumbers.Length, oddNumbers.Length);
+        }
+
+        private void WriteToFile(string fileName, int[] numbers)
+        {
+            File.WriteAllLines(fileName, numbers.Select(n => n.ToString()));
+        }
+
+        private void DisplayStatistics(int evenCount, int oddCount)
+        {
+            StatisticsListBox.Items.Clear();
+            StatisticsListBox.Items.Add($"Кількість парних чисел: {evenCount}");
+            StatisticsListBox.Items.Add($"Кількість непарних чисел: {oddCount}");
         }
     }
 }
